e97f58b696c65d5293a5be924f9e65e452ede5bd
seanazu <87694301+seanazu@users.noreply.github.com>
2021-08-26 11:22:14 +0300

    Update membersController.js

--- MoviesSubscriptionWebSite/Cinema WS/controllers/membersController.js
+++ MoviesSubscriptionWebSite/Cinema WS/controllers/membersController.js
@@ -6,32 +6,32 @@ const axios = require('axios')
 
 
 Router.route('/').get(async (req,resp) =>{
-    let members = await axios.get('http://localhost:3000/members') ;
+    const members = await axios.get('http://localhost:3000/members') ;
     return resp.json(members.data)
 })
 
 Router.route('/:id').get(async (req,resp) =>{
-  let id = req.params.id
-  let member = await axios.get(`http://localhost:3000/members/${id}`) ;
+  const id = req.params.id
+  const member = await axios.get(`http://localhost:3000/members/${id}`) ;
   return resp.json(member.data)
 })
 
 Router.route('/').post(async (req,resp) =>{
-  let obj = req.body 
-  let response = await axios.post('http://localhost:3000/members',obj) 
+  const obj = req.body 
+  const response = await axios.post('http://localhost:3000/members',obj) 
   return resp.json(response.data)
 })
 
 Router.route('/:id').put(async (req,resp) =>{
-  let id = req.params.id
-  let obj = req.body 
-  let response = await axios.put(`http://localhost:3000/members/${id}`,obj) ;
+  const id = req.params.id
+  const obj = req.body 
+  const response = await axios.put(`http://localhost:3000/members/${id}`,obj) ;
   return resp.json(response.data)
  })
 
  Router.route('/:id').delete(async (req,resp) =>{
-   let id = req.params.id 
-   let response = await axios.delete(`http://localhost:3000/members/${id}`)
+   const id = req.params.id 
+   const response = await axios.delete(`http://localhost:3000/members/${id}`)
    return resp.json(response.data)
  })
 

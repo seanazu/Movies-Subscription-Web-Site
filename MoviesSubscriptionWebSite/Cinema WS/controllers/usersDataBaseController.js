0fec31bcaa43195e0141e9a1945f889835c7ca1a
seanazu <87694301+seanazu@users.noreply.github.com>
2021-08-26 11:26:29 +0300

    Update usersDataBaseController.js

--- MoviesSubscriptionWebSite/Cinema WS/controllers/usersDataBaseController.js
+++ MoviesSubscriptionWebSite/Cinema WS/controllers/usersDataBaseController.js
@@ -4,32 +4,32 @@ const Router = express.Router() ;
 const userBL = require('../models/usersDataBaseBL') ;
 
 Router.route('/').get(async (req,resp) =>{
-    let users = await userBL.getAllUsers() ;
+    const users = await userBL.getAllUsers() ;
     return resp.json(users)
 })
 
 Router.route('/:id').get(async (req,resp) =>{
-    let id = req.params.id ;
-    let user = await userBL.getUserById(id) ;
+    const id = req.params.id ;
+    const user = await userBL.getUserById(id) ;
     return resp.json(user) 
 })
 
 Router.route('/').post(async (req,resp) =>{
-    let obj = req.body 
-    let response = await userBL.createNewUser(obj) 
+    const obj = req.body 
+    const response = await userBL.createNewUser(obj) 
     return resp.json(response)
 })
 
 Router.route('/:id').put(async (req,resp) =>{
-    let id = req.params.id 
-    let obj = req.body 
-    let response = await userBL.updateUser(id,obj) ;
+    const id = req.params.id 
+    const obj = req.body 
+    const response = await userBL.updateUser(id,obj) ;
     return resp.json(response)
 })
 
 Router.route('/:id').delete(async (req,resp) =>{
-    let id = req.params.id 
-    let response = await userBL.deleteUser(id) 
+    const id = req.params.id 
+    const response = await userBL.deleteUser(id) 
     return resp.json(response)
 })
 

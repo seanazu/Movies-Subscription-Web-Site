8d429e68db3d8f8812bca9530cfebe8b8909e83d
seanazu <87694301+seanazu@users.noreply.github.com>
2021-08-29 23:58:22 +0300

    Update SubscriptionsUtils.js

--- MoviesSubscriptionWebSite/src/Utils/SubscriptionsUtils.js
+++ MoviesSubscriptionWebSite/src/Utils/SubscriptionsUtils.js
@@ -1,45 +1,45 @@
 import axios from "axios";
 
 const getAllMembers = async () =>{
-    let resp = await axios.get("http://localhost:3001/members")
+    constconst resp = await axios.get("http://localhost:3001/members")
     return resp.data
   }
   const getMemberById = async (id) =>{
-    let resp = await axios.get(`http://localhost:3001/members/${id}`)
+    const resp = await axios.get(`http://localhost:3001/members/${id}`)
     return resp.data
   }
   const postMember = async (obj) =>{
-    let resp = await axios.post(`http://localhost:3001/members`,obj)
+    const resp = await axios.post(`http://localhost:3001/members`,obj)
     return resp.data
   }
   const putMember = async (id,obj) =>{
-      let resp = await axios.put(`http://localhost:3001/members/${id}`,obj)
+      const resp = await axios.put(`http://localhost:3001/members/${id}`,obj)
       return resp.data
   }
   const deleteMember = async (id) =>{
-      let resp = await axios.delete(`http://localhost:3001/members/${id}`)
+      const resp = await axios.delete(`http://localhost:3001/members/${id}`)
       return resp.data
   }
 
   
 const getAllSubscriptions = async () =>{
-    let resp = await axios.get("http://localhost:3001/subscribers")
+    const resp = await axios.get("http://localhost:3001/subscribers")
     return resp.data
   }
   const getSubscriptionById = async (id) =>{
-    let resp = await axios.get(`http://localhost:3001/subscribers/${id}`)
+    const resp = await axios.get(`http://localhost:3001/subscribers/${id}`)
     return resp.data
   }
   const postSubscription = async (obj) =>{
-    let resp = await axios.post(`http://localhost:3001/subscribers`,obj)
+    const resp = await axios.post(`http://localhost:3001/subscribers`,obj)
     return resp.data
   }
   const putSubscription = async (id,obj) =>{
-      let resp = await axios.put(`http://localhost:3001/subscribers/${id}`,obj)
+      const resp = await axios.put(`http://localhost:3001/subscribers/${id}`,obj)
       return resp.data
   }
   const deleteSubscription = async (id) =>{
-      let resp = await axios.delete(`http://localhost:3001/subscribers/${id}`)
+      const resp = await axios.delete(`http://localhost:3001/subscribers/${id}`)
       return resp.data
   }
 

b1ee9a7b1a3ff9b22818108b09ed769b1ee76d84
seanazu <87694301+seanazu@users.noreply.github.com>
2021-08-29 23:57:08 +0300

    Update MoviesUtils.js

--- MoviesSubscriptionWebSite/src/Utils/MoviesUtils.js
+++ MoviesSubscriptionWebSite/src/Utils/MoviesUtils.js
@@ -1,23 +1,23 @@
 import axios from 'axios'
 
 const getAllMovies = async () =>{
-  let resp = await axios.get("http://localhost:3001/movies")
+  const resp = await axios.get("http://localhost:3001/movies")
   return resp.data
 }
 const getMovieById = async (id) =>{
-  let resp = await axios.get(`http://localhost:3001/movies/${id}`)
+  const resp = await axios.get(`http://localhost:3001/movies/${id}`)
   return resp.data
 }
 const postMovie = async (obj) =>{
-  let resp = await axios.post(`http://localhost:3001/movies`,obj)
+  const resp = await axios.post(`http://localhost:3001/movies`,obj)
   return resp.data
 }
 const putMovie = async (id,obj) =>{
-    let resp = await axios.put(`http://localhost:3001/movies/${id}`,obj)
+    const resp = await axios.put(`http://localhost:3001/movies/${id}`,obj)
     return resp.data
 }
 const deleteMovie = async (id) =>{
-    let resp = await axios.delete(`http://localhost:3001/movies/${id}`)
+    const resp = await axios.delete(`http://localhost:3001/movies/${id}`)
     return resp.data
 }
 

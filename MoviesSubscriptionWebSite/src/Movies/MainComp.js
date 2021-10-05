0f1e544d2e34ff70f3c80196492caf8835c8443c
seanazu <87694301+seanazu@users.noreply.github.com>
2021-10-05 23:43:47 +0300

    Update MainComp.js

--- MoviesSubscriptionWebSite/src/Movies/MainComp.js
+++ MoviesSubscriptionWebSite/src/Movies/MainComp.js
@@ -9,13 +9,12 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 
 const MainComp = () => {
-    const[findInput, setInput] = useState("")
+    const[findInput, setInput] = useState('')
     const[createMovieDisplay,setDisplay]=useState('')
 
     useEffect(()=>{
         history.push('/mainpage/movies/allMovies')
-        let addMovieDisplay = localStorage.getItem('createMovies')
-        console.log(addMovieDisplay)
+        const addMovieDisplay = localStorage.getItem('createMovies')
         setDisplay(addMovieDisplay)
     },[])
 
